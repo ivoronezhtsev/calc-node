5afbcadd01ab3c151812e063ad2037a69ffa3e9d
igor <i.voronezhtsev@gmail.com>
2024-01-21 23:10:11 +0300

    убрал лишнюю пустую строку

--- calc.js
+++ calc.js
@@ -2,7 +2,6 @@ const operand1 = process.argv[2]
 const operator = process.argv[3]
 const operand2 = process.argv[4]
 
-
 let result;
 switch (operator) {
     case "+": result = +operand1 + +operand2; break;
